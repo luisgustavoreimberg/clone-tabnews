e85a142cb3b50df6bf2c8c19ff81c64a7498b02d
Luis Gustavo Gonçalves Reimberg <55925584+luisgustavoreimberg@users.noreply.github.com>
2024-05-16 00:58:44 +0000

    Atualizando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Curso Dev</h1>
+    return <div>
+                <h1>Desafio Curso Dev</h1>
+                <span>com Felipe Dechamps</span>
+            </div>
 }
 
 export default Home;
